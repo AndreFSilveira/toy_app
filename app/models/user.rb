760bc869562e76e6642ba0945b62b0134a00235a
André Felipe Silveira <andrefelipesilveira@yahoo.com.br>
2015-03-27 13:01:25 -0300

    validação de email, name e conteudo do post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	validates :name, presence: true
+	validates :name, uniqueness: true
 end
